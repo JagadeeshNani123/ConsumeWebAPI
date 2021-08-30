7ae1eb827b7a5e5e50ff0308e1239b8e72d227be
JagadeeshNani123 <virupakshijagadeeshreddyv@gmail.com>
2021-08-30 19:45:49 +0530

    Add project files.

---
+++ ConsumeWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using ConsumeWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsumeWebAPI.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly HttpClient client = null;
+        private string employeesApiUrl = "";
+        public EmployeeController(HttpClient client, IConfiguration config)
+        {
+            this.client = client;
+            employeesApiUrl = config.GetValue<string>("AppSettings:EmployeesApiUrl");
+        }
+        public async Task<IActionResult> Index()
+        {
+            HttpResponseMessage response = await client.GetAsync(employeesApiUrl);
+            string stringdata = await response.Content.ReadAsStringAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            List<Employee> data = JsonSerializer.Deserialize<List<Employee>>(stringdata, options);
+
+            return View(data);
+        }
+    }
+}
---
+++ ConsumeWebAPI/Models/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsumeWebAPI.Models
+{
+    public class Employee
+    {
+        
+        [Required(ErrorMessage = "Employee Id is required")]
+        [Display(Name = "Employee Id")]
+        public int EmployeeID { set; get; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        [Display(Name = "First Name")]
+        [StringLength(10, ErrorMessage = "First name must be less than 10 characters")]
+        public string FirstName { set; get; }
+
+        [Required(ErrorMessage = "Employyee Name is required")]
+        [Display(Name = "Last Name")]
+        [StringLength(20, ErrorMessage = "Last name must be less than 20 characters")]
+        public string LastName { set; get; }
+
+        [Required(ErrorMessage = "Title Name is required")]
+        [Display(Name = "Title")]
+        [StringLength(20, ErrorMessage = "Title must be less than 20 characters")]
+        public string Title { set; get; }
+
+        [Required(ErrorMessage = "Birth date is required")]
+        [Display(Name = "Birth Date")]
+        public DateTime BirthDate { set; get; }
+
+        [Required(ErrorMessage = "Hire Date is required")]
+        [Display(Name = "Hire Date")]
+        public DateTime HireDate { set; get; }
+
+        [Required(ErrorMessage = "Notes Name is required")]
+        [Display(Name = "Notes")]
+        [StringLength(500, ErrorMessage = "Notes must be less than 500 characters")]
+        public string Notes { set; get; }
+
+    }
+}
